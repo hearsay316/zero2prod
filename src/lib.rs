d2e84081cd829445589e00fec61b2a93f90e7260
hearsay316 <hearsay316@gmail.com>
2024-08-12 15:39:11 +0800

    feat: subscriptions 接受表单参数调用

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,14 @@ use std::net::TcpListener;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use actix_web::dev::Server;
 
+#[derive(serde::Deserialize)]
+struct FormData{
+    name:String,
+    email:String
+}
+// fn index(form:web::Form<FormData>)->String{
+//     format!("Welcome {}!",form.username)
+// }
 async fn health_check() -> HttpResponse {
     // HttpResponse::Ok().finish()
     HttpResponse::Ok().finish()
@@ -11,11 +19,15 @@ async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("世界1");
     format!("hello {} !", &name)
 }
+async fn subscribe(_form:web::Form<FormData>)->HttpResponse{
+    HttpResponse::Ok().finish()
+}
 
 pub  fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
   let server =   HttpServer::new(|| {
         App::new().route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions",web::post().to(subscribe))
             .route("/{name}", web::get().to(greet))
     }).listen(listener)?
         .run();
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -9,21 +9,21 @@ use reqwest::Client;
 //
 // You can inspect what code gets generated using
 // `cargo expand --test health_check` (<- name of the test file)
-fn spawn_app()->String {
+fn spawn_app() -> String {
     print!("开始了");
     let listener = TcpListener::bind("127.0.0.1:0").expect("一个错误:port");
-    println!("{:?}",listener);
-    let  port = listener.local_addr().unwrap().port();
+    println!("{:?}", listener);
+    let port = listener.local_addr().unwrap().port();
 
     let server = zero2prod::run(listener).expect("这个是一个错误");
     let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}",port)
+    format!("http://127.0.0.1:{}", port)
 }
 #[tokio::test]
 async fn health_check_works() {
-   let address =  spawn_app();
+    let address = spawn_app();
     let client = Client::new();
-    let response = client.get(&format!("{}/health_check",address))
+    let response = client.get(&format!("{}/health_check", address))
         .send()
         .await
         .expect(" Failed to execute request");
@@ -33,8 +33,39 @@ async fn health_check_works() {
 }
 
 
-// `tokio::test` is the testing equivalent of `tokio::main`.
-// It also spares you from having to specify the `#[test]` attribute.
-//
-// You can inspect what code gets generated using
-// `cargo expand --test health_check` (<- name of the test file)
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    // art
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client.post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    assert_eq!(200, response.status().as_u16());
+}
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![("name=le%20guin", "missing the email"),
+                          ("email=ursula_le_guin%40gmail.com", "missing the name"),
+                          ("", "missing both name email")
+    ];
+    for (invalid_body, error_message) in test_cases {
+        let response = client.post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(400, response.status().as_u16(),
+                   "The API did not fail with 400 Bad Request when the payload was {}.",
+                   error_message
+        );
+    }
+}
